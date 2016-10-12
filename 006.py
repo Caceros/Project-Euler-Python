b7c3ec5e595d2efbdc5e15171b30985ef2cb375c
Caceros <caceros995@gmail.com>
2016-10-12 22:38:58 +0800

    Easy.

---
+++ 006.py
@@ -0,0 +1,22 @@
+"""
+Project Euler Problem 6
+=======================
+
+The sum of the squares of the first ten natural numbers is,
+                       1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+                    (1 + 2 + ... + 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten
+natural numbers and the square of the sum is 3025 - 385 = 2640.
+
+Find the difference between the sum of the squares of the first one
+hundred natural numbers and the square of the sum.
+"""
+
+import numpy as np
+num = range(1, 101)
+np_num = np.array(num)
+print sum(num)**2 - sum(np_num**2)
+    
